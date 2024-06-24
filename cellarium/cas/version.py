0f3b904dfa42b553a0eecd62d27bfc3400ed8691
nmalfroy <nmalfroy@broadinstitute.org>
2024-06-24 12:47:03 -0400

    Make version check backwards compatible

--- cellarium/cas/version.py
+++ cellarium/cas/version.py
@@ -1,18 +1,31 @@
 import os
 import re
-from importlib.metadata import PackageNotFoundError, version
+from sys import version_info as python_version_info
 
 
 def get_version() -> str:
     """
-    Get the version of the application base on git tag.
+    Get the version of the application based on the git tag.
 
     :return: The version of the application.
     """
-    try:
-        return version("cellarium-cas")
-    except PackageNotFoundError:
-        return os.environ.get("CAS_VERSION", "0.0.1")
+
+    if python_version_info.major == 3 and python_version_info.minor >= 8:
+        from importlib.metadata import PackageNotFoundError, version
+
+        try:
+            return version("cellarium-cas")
+        except PackageNotFoundError:
+            return os.environ.get("CAS_VERSION", "0.0.1")
+    elif python_version_info.major == 3 and python_version_info.minor == 7:
+        from importlib_metadata import PackageNotFoundError, version
+
+        try:
+            return version("cellarium-cas")
+        except PackageNotFoundError:
+            return os.environ.get("CAS_VERSION", "0.0.1")
+    else:
+        raise Exception("Unsupported Python version. Please use Python 3.7 or later.")
 
 
 def _is_release_version(version_string) -> bool:
