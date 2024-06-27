d2caf1b64d9948299d8a8aa5e7e836ef135fd1aa
Kevin Lydon <klydon@broadinstitute.org>
2024-06-27 11:23:47 -0400

    Included description in the info that gets printed for models

--- cellarium/cas/settings/development.py
+++ cellarium/cas/settings/development.py
@@ -1,1 +1,2 @@
-CELLARIUM_CLOUD_BACKEND_URL = "https://cellarium-cloud-api-dev.cellarium.ai"
+# CELLARIUM_CLOUD_BACKEND_URL = "https://cellarium-cloud-api-dev.cellarium.ai"
+CELLARIUM_CLOUD_BACKEND_URL = "http://localhost:8000"
