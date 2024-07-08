7189469c984554261abc6332663f2d2c1d10b118
nmalfroy <nmalfroy@broadinstitute.org>
2024-07-08 14:37:44 -0400

    Expose the CASClient class

--- cellarium/cas/__init__.py
+++ cellarium/cas/__init__.py
@@ -1,8 +1,10 @@
+from cellarium.cas.client import CASClient
 from . import constants, exceptions, postprocessing, preprocessing, service, settings, version, visualization
 
 __version__ = version.get_version()
 
 __all__ = [
+    "CASClient",
     "preprocessing",
     "postprocessing",
     "visualization",
