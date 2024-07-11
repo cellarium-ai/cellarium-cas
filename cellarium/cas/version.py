1f23645f0b393f1638b2ebff1d15ff50d9ffcbe1
Kevin Lydon <klydon@broadinstitute.org>
2024-07-11 10:18:20 -0400

    Using __package__ is probably fine, but using cellarium-cas worked fine too, so I'm just gonna go back to that instead of changing it in a mostly-unrelated PR

--- cellarium/cas/version.py
+++ cellarium/cas/version.py
@@ -14,7 +14,7 @@ def get_version() -> str:
         from importlib.metadata import PackageNotFoundError, version
 
         try:
-            return version(__package__)
+            return version("cellarium-cas")
         except PackageNotFoundError:
             return os.environ.get("CAS_VERSION", "0.0.1")
     elif python_version_info.major == 3 and python_version_info.minor == 7:
