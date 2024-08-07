4563ed0fb786cb12976fd49a39d2fe9bd427a5bb
nmalfroy <nmalfroy@broadinstitute.org>
2024-08-07 18:14:31 -0400

    Simplify doc configs
    - Update the copyright to the current year (was hardcoded to 2023)
    - Remove unused options
    - Add the version as an explicit option
    - Use an extension to do code substitution of version and remove the template_installation file

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,15 +2,17 @@
 #
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
-import os
+import time
 from setuptools_git_versioning import get_tag
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = "Cellarium CAS"
-copyright = "2023, Cellarium AI"
+copyright = f"{time.strftime('%Y')}, Cellarium AI"
 author = "Cellarium AI"
+version = get_tag()
+release = get_tag()
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -20,10 +22,17 @@ extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
     "sphinx.ext.viewcode",
+    "sphinx_substitution_extensions",
 ]
 
-templates_path = ["_templates"]
-exclude_patterns = ["**/installation_template.rst"]
+# Provide substitutions for common values
+rst_prolog = f"""
+.. |project| replace:: {project}
+.. |copyright| replace:: {copyright}
+.. |author| replace:: {author}
+.. |version| replace:: {version}
+.. |release| replace:: {release}
+"""
 
 rst_epilog = """
 .. |br| raw:: html
@@ -35,28 +44,3 @@ rst_epilog = """
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = "sphinx_rtd_theme"
-html_static_path = ["_static"]
-
-
-def substitute_installation_version():
-    """
-    Generate /modules/installation_.rst from /modules/installation.rst by substituting '|cas_version|' with a tagged
-    version of a library.
-    This step was necessary because the Sphinx builder couldn't replace the '|cas_version|' variable with a value
-    inside a code block.
-    """
-    dir_path = os.path.dirname(os.path.realpath(__file__))
-    cellarium_cas_version = get_tag()
-
-    with open(f"{dir_path}/modules/installation_template.rst", "r") as f:
-        content = f.read()
-
-    # Replace the placeholder with the version
-    content = content.replace("|cas_version|", cellarium_cas_version)
-
-    # Write the processed content to the actual .rst file
-    with open(f"{dir_path}/modules/installation.rst", "w") as f:
-        f.write(content)
-
-
-substitute_installation_version()
